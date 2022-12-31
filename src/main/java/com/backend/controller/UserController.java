1add2f7a368fa035ebb261eb6fb154443447de5e
unknown <al4nlaz0@gmail.com>
2022-12-31 19:11:08 -0300

    update UserRepository

--- src/main/java/com/backend/controller/UserController.java
+++ src/main/java/com/backend/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins= "http://localhost:4200/")
 public class UserController {
 
     @Autowired
@@ -20,6 +21,7 @@ public class UserController {
     @PostMapping("/login")
     public boolean login(@RequestBody LoginDto data){
         boolean existe=false;
+        System.out.println(data.getEmail()+"=="+data.getPassword());
         if(data.getEmail()!= null && data.getPassword()!=null){
         existe =  userRepository.existsByEmailAndPassword(data.getEmail(), data.getPassword());
          System.out.println(existe);
@@ -38,11 +40,12 @@ public class UserController {
     }
 
     @PutMapping("/actualizar/{id}")
-    public void actualizarDatos(@PathVariable("id")Long id,@RequestBody LoginDto data){
+    public User actualizarDatos(@PathVariable("id")Long id,@RequestBody LoginDto data){
         User user = userService.getUser(id);
         user.setEmail(data.getEmail());
         user.setPassword(data.getPassword());
         userService.updateUser(user);
+        return user;
     }
 }
 
--- src/main/java/com/backend/repository/UserRepository.java
+++ src/main/java/com/backend/repository/UserRepository.java
@@ -3,13 +3,14 @@ package com.backend.repository;
 import com.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    @Query(value = "select id from user where email LIKE %1 and password=%2", nativeQuery = true)
-    boolean login(String email, String password);
+    @Query(value = "SELECT * FROM user WHERE email LIKE ':email' and password LIKE ':password'", nativeQuery = true)
+    User getUsuario(@Param("email")String email,@Param("password")String password);
 
     boolean existsByEmailAndPassword(String email,String password);
 }
