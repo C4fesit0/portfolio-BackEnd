39d431f695a4c983e46b5e5f0faf4b6581fe6138
Alan Lazo <al4nlaz0@gmail.com>
2023-03-08 22:01:36 -0300

    docker file agregado - Canada

--- src/main/java/com/backend/controller/NivelEstudioController.java
+++ src/main/java/com/backend/controller/NivelEstudioController.java
@@ -2,17 +2,17 @@ package com.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.backend.model.NivelEstudio;
 import com.backend.service.INivelEstudioService;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/nivelEstudio")
+//@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://port-front.firebaseapp.com/home/")
 public class NivelEstudioController {
     
     @Autowired
